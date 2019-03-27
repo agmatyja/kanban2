7fa9aee496ecce70402ce1f682f8a2465a103951
Agnieszka Matyja <agmatyja@gmail.com>
2019-03-27 22:21:08 +0100

    cards improved

--- App.js
+++ App.js
@@ -13,18 +13,20 @@ fetch(prefix + baseUrl + '/board', { headers: myHeaders })
   .then(function(resp) {
 	console.log(resp);  
     setupColumns(resp.columns);
+	
   });
 function setupColumns(columns) {
 	columns.forEach(function(column) {
 		var col = new Column(column.id, column.name);
 		board.addColumn(col);
+		setupCards(col, column.cards);
 	});
 }
   
 function setupCards(col, cards) {
 	cards.forEach(function (card) {
-    var cardObj = new Card(card.id, card.name);
-  	col.addCard(cardObj);
+		var cardObj = new Card(card.id, card.name);
+		col.addCard(cardObj);
 	});
 }
 // OGÓLNA FUNKCJA
--- Card.js
+++ Card.js
@@ -3,7 +3,7 @@ function Card(id, name) {
   	var self = this;
 
   	this.id = id;
-  	this.name = name; /*|| 'No name given';*/
+  	this.name = name || 'No name given';
   	this.element = generateTemplate('card-template', { description: this.name }, 'li');
 
   	this.element.querySelector('.card').addEventListener('click', function (event) {
